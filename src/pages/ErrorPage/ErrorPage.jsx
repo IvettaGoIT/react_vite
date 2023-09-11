f1fb79e36b15544a4edcd61e9cb4b457069ee4c0
Ivetta Dashkova <ivettafsd@gmail.com>
2023-09-11 13:31:12 +0200

    delete images

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,7 @@
-import glass from '../../images/porto-glass.jpg';
-
 const ErrorPage = () => {
   return (
     <div>
-      <h2>4</h2>
-      <img src={glass} alt="" />
-      <h2>4</h2>
+      <h2>4</h2>0<h2>4</h2>
     </div>
   );
 };
