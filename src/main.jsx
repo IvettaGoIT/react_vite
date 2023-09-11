9bbd74987f5bb6881cbfe5f8a25b4eb0ae8c6ff5
Ivetta Dashkova <ivettafsd@gmail.com>
2023-09-11 10:31:26 +0200

    add correct basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-with-vite/">
+    <BrowserRouter basename="/react_vite">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
