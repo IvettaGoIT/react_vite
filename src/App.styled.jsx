5c618ed79df98be19b50a55d752d032186dc54f8
Ivetta Dashkova <ivettafsd@gmail.com>
2023-10-16 20:50:23 +0200

    fix

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,10 +1,11 @@
 import styled from '@emotion/styled';
-import example from './assets/example.png';
+import example from './assets/Background.png';
 
 export const AppWrapper = styled.div`
   width: 100%;
   height: 100vh;
   background-image: url(${example});
   background-repeat: no-repeat;
-  background-position: center bottom;
+  background-position: left top;
+  background-size: 100% 100%;
 `;
