84fef73fc33318feb7e1e21cffb37abc4961537b
Ivetta Dashkova <ivettafsd@gmail.com>
2023-10-20 12:38:05 +0200

    fix file name

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import example from './assets/Background.png';
+import example from './assets/background.png';
 
 export const AppWrapper = styled.div`
   width: 100%;
