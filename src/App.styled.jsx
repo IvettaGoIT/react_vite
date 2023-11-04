0cd552ac202865d5adc330047d3fd6b44e3271b4
Ivetta Dashkova <ivettafsd@gmail.com>
2023-11-04 14:27:26 +0100

    test

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import example from './assets/background.png';
+import 'normalize.css';
 
 export const AppWrapper = styled.div`
   width: 100%;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.jsx'; 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
