936649ff413a66a56b6338e9334d39ab1fecb964
IvettaDashkova <ivettafsd@gmail.com>
2023-12-01 11:43:15 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(({ command }) => {
-  
-    const config = {
+export default defineConfig({
       plugins: [react(), svgr()],
       resolve: {
         alias: {
@@ -14,10 +12,5 @@ export default defineConfig(({ command }) => {
           assets: '/src/assets'
         },
       },
-      base: '/',
-    }
-   if (command !== 'serve' ) { 
-    config. base = '/react_vite/'
-   } 
-  return config
-});
+      base: '/react_vite/',
+    });
