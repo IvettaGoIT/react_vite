41ce07186d113cb524f48d7ce1eb3e1ef8caba74
IvettaDashkova <ivettafsd@gmail.com>
2023-12-01 11:01:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import './index.css';
-
+// test
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/react_vite">
